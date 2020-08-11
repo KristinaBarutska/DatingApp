f0144990307354ac5fa6ac5d88a20d912904ea88
Kristina Barutska <kristina.barutska@microfocus.com>
2020-08-11 11:06:07 +0300

    Finished frontend login and registration functionality

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,12 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = { };
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
   login(){
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next =>{
+      console.log('Bravo!');
+    }, error => {
+      console.log('Error!');
+    });
+  }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    console.log('Logged out!');
   }
 
 }
