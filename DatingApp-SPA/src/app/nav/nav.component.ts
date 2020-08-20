db9e5ac1c9b776ad9ba37eac938a77cd5b2f4f44
Kristina Barutska <kristina.barutska@microfocus.com>
2020-08-20 12:37:05 +0300

    Added routing

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,26 +10,32 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class NavComponent implements OnInit {
   model: any = { };
-  constructor(public authService: AuthService, private alerfity: AlertifyService) { }
+  constructor(public authService: AuthService, private alerfity: AlertifyService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line: typedef
   login(){
-    this.authService.login(this.model).subscribe(next =>{
+    this.authService.login(this.model).subscribe(next => {
       this.alerfity.success('You logged in successfuly');
     }, error => {
       this.alerfity.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
+  // tslint:disable-next-line: typedef
   loggedIn(){
     return this.authService.loggedIn();
   }
 
+  // tslint:disable-next-line: typedef
   logout(){
     localStorage.removeItem('token');
     this.alerfity.message('You are logged out');
+    this.router.navigate(['/home']);
   }
 
 }
